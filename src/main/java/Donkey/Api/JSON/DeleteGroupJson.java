6cbd538ce42d283f71a4dbc53c0fb620582ee776
Matthieu Gorce <matthieu.gorce@imerir.com>
2019-02-14 14:48:36 +0100

    Error management in  api Group

--- src/main/java/Donkey/Api/JSON/DeleteGroupJson.java
+++ src/main/java/Donkey/Api/JSON/DeleteGroupJson.java
@@ -1,17 +1,22 @@
 package Donkey.Api.JSON;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 public class DeleteGroupJson {
     public int id;
     public String name;
     public int parentId;
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+    public String message;
 
     public DeleteGroupJson(){
 
     }
 
-    public DeleteGroupJson(int id, String name, int parentId){
+    public DeleteGroupJson(int id, String name, int parentId, String message){
         this.id = id;
         this.name = name;
         this.parentId = parentId;
+        this.message = message;
     }
 }
--- src/main/java/Donkey/Api/JSON/GroupJson.java
+++ src/main/java/Donkey/Api/JSON/GroupJson.java
@@ -1,16 +1,21 @@
 package Donkey.Api.JSON;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 public class GroupJson {
     public String name;
     public int parent;
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+    public String message;
 
 
     public GroupJson(){
 
     }
 
-    public GroupJson(String name, int parent){
+    public GroupJson(String name, int parent,String message){
         this.name = name;
         this.parent = parent;
+        this.message = message;
     }
 }
