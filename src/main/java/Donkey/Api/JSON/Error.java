ffff08d41a27cf017955df12058fc91e23b27ce6
Sebastien <seb6596@gmail.com>
2019-02-18 14:03:15 +0100

    Add code field on error json

--- src/main/java/Donkey/Api/JSON/Error.java
+++ src/main/java/Donkey/Api/JSON/Error.java
@@ -1,10 +1,12 @@
 package Donkey.Api.JSON;
 
 public class Error {
+    public String code;
     public String message;
 
-    public Error(String message) {
+    public Error(String message, String code) {
         this.message = message;
+        this.code = code;
     }
 
     public Error() {
