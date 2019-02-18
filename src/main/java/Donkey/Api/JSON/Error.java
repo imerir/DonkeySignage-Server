d7f0fc11487fa382ac6d6934f30023daa2c2af34
Sebastien <seb6596@gmail.com>
2019-02-18 11:34:04 +0100

    Add error constructor

--- src/main/java/Donkey/Api/JSON/Error.java
+++ src/main/java/Donkey/Api/JSON/Error.java
@@ -2,4 +2,11 @@ package Donkey.Api.JSON;
 
 public class Error {
     public String message;
+
+    public Error(String message) {
+        this.message = message;
+    }
+
+    public Error() {
+    }
 }
