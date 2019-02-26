ac9aff837ca46060ada126b8cd986975acc8e24a
Matthieu Gorce <matthieu.gorce@imerir.com>
2019-02-26 11:32:30 +0100

    File Upload System
    TODO : add several file in same time

--- src/main/java/Donkey/MainClass.java
+++ src/main/java/Donkey/MainClass.java
@@ -1,14 +1,26 @@
 package Donkey;
 
+import Donkey.Storage.Service.StorageService;
+import Donkey.Storage.StorageProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class MainClass {
-
-
     public static void main(String args[]){
         SpringApplication.run(MainClass.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
