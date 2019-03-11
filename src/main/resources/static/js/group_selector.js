f5e136891faaddd85cfeff45971a8da327a93f52
Sebastien <seb6596@gmail.com>
2019-03-11 14:18:22 +0100

    Finish screen register page

--- src/main/resources/static/js/group_selector.js
+++ src/main/resources/static/js/group_selector.js
@@ -1,6 +1,6 @@
 var groupSelectorDiv;
 
-var parent = -1
+var parent = -1;
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -13,16 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
 function getGroupPage(groupId) {
     console.log(groupId);
     $.get("/api/group/getChildren?id="+groupId, (response)=>{
-        console.log(response);
         groupSelectorDiv.empty();
         if(groupId != -1)
-            groupSelectorDiv.append("<li class='collection-item'><a href='#' data-id='"+response.parentId+"' class='changeGroupBtn'>...<i class='material-icons'>arrow_upward</i></a> </li>");
+            groupSelectorDiv.append("<li class='collection-item'><a href='#' data-id='"+response.parentId+"' class='changeGroupBtn'><i class='material-icons'>arrow_back</i></a> </li>");
         response.children.forEach((el)=>{
             console.log(el);
             groupSelectorDiv.append("" +
                 "<li class=\"collection-item\">" +
                 "<div>" +
-                "<a href='#' class='changeGroupBtn' data-id='"+ el.id+"'>"+ el.name+"</a><a href='#' data-id='"+ el.id+"' class='secondary-content'><i class='material-icons'>send</i></a>" +
+                "<a href='#' class='changeGroupBtn' style='font-size: 18px' data-id='"+ el.id+"'>" +
+                "<i class='material-icons left grey-text' style='margin-right: 5px'>folder</i>"+ el.name+"</a>" +
+                "<a href='#' data-id='"+ el.id+"' data-name='"+el.name+"' class='secondary-content selectGroupBtn'><i class='material-icons green-text'>exit_to_app</i>" +
+                "</a>" +
                 "</div>" +
                 "</li>");
         });
@@ -38,5 +40,22 @@ function listeners(){
     $(".changeGroupBtn").click(function(){
         console.log($(this).attr("data-id"));
         getGroupPage($(this).attr("data-id"));
+    });
+
+    $(".selectGroupBtn").click(function(){
+        console.log($(this).attr("data-id"));
+        console.log($(this).attr("data-name"));
+        selectGroup($(this).attr("data-id"), $(this).attr("data-name"));
+        var instance = M.Modal.getInstance($("#modalGroup").get(0));
+        instance.close();
     })
+
+
+
+}
+
+
+function selectGroup(id, name) {
+    $('#groupId').val(id);
+    $('#groupName').val(name);
 }
