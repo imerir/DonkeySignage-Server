01f4c49a2e4ba9ac32a57941d364475d11ecfc4a
Sebastien <seb6596@gmail.com>
2019-03-18 10:36:19 +0100

    remove delete upload dir on start up

--- src/main/java/Donkey/MainClass.java
+++ src/main/java/Donkey/MainClass.java
@@ -19,7 +19,6 @@ public class MainClass {
     @Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> {
-            storageService.deleteAll();
             storageService.init();
         };
     }
