c6d35780adde89be1dbb0cf3eb25a3b445ce70c8
Matthieu Gorce <matthieu.gorce@imerir.com>
2019-03-26 16:44:55 +0100

    New Widget
    Add jquery rss librairy
    Create class Rss
    Create rss html

---
+++ src/main/java/Donkey/Widgets/Rss.java
@@ -0,0 +1,42 @@
+package Donkey.Widgets;
+
+import Donkey.Tools.FilesTools;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Rss implements WidgetInterface {
+    private Logger logger = LogManager.getLogger();
+
+    @Override
+    public String getName() {
+        return "RSS";
+    }
+
+    @Override
+    public String getId() {
+        return "RSS";
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
+
+    @Override
+    public String getTemplate() throws IOException {
+        return FilesTools.getINSTANCE().getFileContent("widgets/image_slides.html");
+    }
+
+    @Override
+    public String convertParam(String paramStr) throws IOException {
+        return null;
+    }
+
+    @Override
+    public List<WidgetConfDefinition> getParam() {
+        return null;
+    }
+}
